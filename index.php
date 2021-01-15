8e2269e9b21de01c154c40e27c44686c1fffd054
Nitinie <beerolo01@gmail.com>
2021-01-15 14:38:57 +0700

    edit index

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 echo "My Hompaeg";
 ?>
-<a href="page2.php">page1</a>
+<a href="page1.php">page1</a>
 <a href="page2.php">page2</a>
-<a href="page2.php">page3</a>
+<a href="page3.php">page3</a>
+<a href="page4.php">page4</a>
+<a href="page5.php">page5</a>
